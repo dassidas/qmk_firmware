264401b46b3b6efb8c5be2bbd7b9235c3c02a0f7
alexjhannan <alexjhannan@gmail.com>
2021-01-25 18:10:13 -0500

    make mouse keymap ambidexterous

--- keyboards/planck/keymaps/alexjhannan/keymap.c
+++ keyboards/planck/keymaps/alexjhannan/keymap.c
@@ -190,10 +190,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_MOUSE] = LAYOUT_planck_grid(
-    KC_TAB,  _______,    _______,    _______,    _______,    _______,    _______,    KC_MS_BTN1,  KC_MS_U,   KC_MS_BTN2,   _______,    _______,
-    KC_ESC,  _______,    _______,    _______,    _______,    _______,    _______,    KC_MS_L,     KC_MS_D,   KC_MS_R,    _______, _______,
-    KC_LSFT, KC_ACL0,    KC_ACL1,    KC_ACL2,    KC_MS_BTN4,    KC_MS_BTN5,    _______,    KC_WH_D,     _______,   KC_WH_U,    KC_ACL2, _______ ,
-    QWERTY,  KC_LCTL, KC_LALT, KC_LGUI, QWERTY,   KC_MS_BTN2,  KC_MS_BTN1,     RAISE,       KC_WH_L,   KC_WH_D,    KC_WH_U,   KC_WH_R
+    KC_TAB,   KC_MS_BTN1, KC_MS_U,    KC_MS_BTN2, _______,       _______,    _______,       KC_MS_BTN1,  KC_MS_U,   KC_MS_BTN2, _______,   _______,
+    KC_ACL2,   KC_MS_L,    KC_MS_D,    KC_MS_R,    KC_MS_BTN5,    _______,    KC_MS_BTN5,    KC_MS_L,     KC_MS_D,   KC_MS_R,    _______,   _______,
+    KC_LSFT,  KC_WH_D,    _______,    KC_WH_U,    KC_MS_BTN4,    _______,    KC_MS_BTN4,    KC_WH_D,     _______,   KC_WH_U,    KC_ACL2,   _______ ,
+    QWERTY,   KC_LCTL,    KC_LALT,    KC_LGUI,    KC_MS_BTN1,    KC_MS_BTN2, KC_MS_BTN2,    KC_MS_BTN1,  KC_WH_L,   KC_WH_D,    KC_WH_U,   KC_WH_R
 )
 
 };
